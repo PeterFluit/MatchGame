82ebd838bb68ba0a697c8402776d87b6b78a03bb
Peter Fluit <fluitpd@gmail.com>
2022-09-14 20:01:40 -0600

    Draggable Behavior and Dragging the Object

---
+++ ColorMatch/Assets/DraggableBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableBehavior : MonoBehaviour
+{
+
+    private Camera cameraObj;
+    public bool draggable;
+    public Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObj = Camera.main;
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        draggable = true;
+
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+            position = cameraObj.ScreenToViewportPoint(Input.mousePosition);
+            transform.position = position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        draggable = false;
+    }
+
+}
