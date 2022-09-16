42196934df3c12769b9c7eca4ef9daf1518bd232
Peter Fluit <fluitpd@gmail.com>
2022-09-15 21:48:56 -0600

    Setting Score Labels
    And consulting Anthony's video on using TextMeshPro

--- ColorMatch/Assets/IntData.cs
+++ ColorMatch/Assets/IntData.cs
@@ -6,6 +6,11 @@ public class IntData : ScriptableObject
 {
     public int value;
 
+    public void SetValue(int num)
+    {
+        value = num;
+    }
+
     public void UpdateValue(int number)
     {
         value += number;
