4464e73d0fc0203e6c5e1c89b6295dfe0db43349
Peter Fluit <fluitpd@gmail.com>
2022-09-20 17:15:11 -0600

    Go! Part2 and 3

--- ColorMatch/Assets/Scripts/CoroutineBehaviour.cs
+++ ColorMatch/Assets/Scripts/CoroutineBehaviour.cs
@@ -5,9 +5,9 @@ using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    public UnityEvent repeatEvent;
+    public UnityEvent startEvent, repeatEvent, endEvent;
 
-    public int counterNum = 3;
+    public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
@@ -18,13 +18,14 @@ public class CoroutineBehaviour : MonoBehaviour
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-
-        while (counterNum > 0)
+        startEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
         {
-            yield return wfsObj;
             repeatEvent.Invoke();
-
+            counterNum.value--;
+            yield return wfsObj;
         }
-
+        endEvent.Invoke();
     }
 }
