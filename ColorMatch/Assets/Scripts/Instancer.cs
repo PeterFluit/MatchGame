967212f67d5aff48a4242664017bc53a36389a5b
Peter Fluit <fluitpd@gmail.com>
2022-09-20 20:56:15 -0600

    Instancing with a Count
    till 6 minute mark

--- ColorMatch/Assets/Scripts/Instancer.cs
+++ ColorMatch/Assets/Scripts/Instancer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Instancer : ScriptableObject
 {
     public GameObject prefab;
+    private int num;
 
    public void CreateInstance ()
    {
@@ -18,9 +19,19 @@ public class Instancer : ScriptableObject
 
    public void CreateInstanceFromList(Vector3DataList obj)
    {
-    foreach (var i = 0; i < obj.vector3DList.Count; i++)
+    foreach (var t:Vector3Data in obj.vector3DList)
     {
-        Instantiate(prefab, obj.vector3DList[i].value, Quaternion.identity);
+        Instantiate(prefab, t.value, Quaternion.identity);
     }    
    }
+
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+   {
+    Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
+    num++;
+    if(num == obj.vector3DList.Count)
+    {
+        num = 0;
+    }
+   }    
 }
