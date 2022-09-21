847f8d3334338c1cfa0fae0c26f09004f1ffb676
Peter Fluit <fluitpd@gmail.com>
2022-09-20 20:06:14 -0600

    Vector3data

--- ColorMatch/Assets/Scripts/Instancer.cs
+++ ColorMatch/Assets/Scripts/Instancer.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Instancer : ScriptableObject
 {
-   public void CreateInstance (GameObject obj)
+    public GameObject prefab;
+
+   public void CreateInstance ()
    {
-    Instantiate(obj);
+    Instantiate(prefab);
    }
+
+   public void CreateInstance (Vector3Data obj)
+   {
+    Instantiate(prefab, obj.value, Quaternion.identity);
+   }
+
 }
