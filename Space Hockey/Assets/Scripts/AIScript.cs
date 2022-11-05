1f5a2c3a4e231b8f307dfea38fcbf5fab374fd6a
Peter Fluit <fluitpd@gmail.com>
2022-11-05 14:28:12 -0600

    Space Hockey edits
    also got anither assignment, Leo, stuck in by mistake

---
+++ Space Hockey/Assets/Scripts/AIScript.cs
@@ -0,0 +1,58 @@
+//Code from Reso Coder https://www.youtube.com/watch?v=67Seb_GJlpc&t=191s&ab_channel=ResoCoder
+
+using UnityEngine;
+
+public class AIScript : MonoBehaviour
+{
+    public float MaxMovementSpeed;
+    private Rigidbody rb;
+    private Vector2 startingPosition;
+
+    public Rigidbody Puck;
+
+    public Transform PlayerBoundaryHolder;
+    private Boundary playerBoundary;
+
+    public Transform PuckBoundaryHolder;
+    private Boundary puckBoundary;
+
+    private Vector2 targetPosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startingPosition = rb.position;
+        
+        playerBoundary = new Boundary(PlayerBoundaryHolder.GetChild(0).position.y,
+            PlayerBoundaryHolder.GetChild(1).position.y,
+            PlayerBoundaryHolder.GetChild(2).position.x,
+            PlayerBoundaryHolder.GetChild(3).position.x);
+        
+        playerBoundary = new Boundary(PuckBoundaryHolder.GetChild(0).position.y,
+            PuckBoundaryHolder.GetChild(1).position.y,
+            PuckBoundaryHolder.GetChild(2).position.x,
+            PuckBoundaryHolder.GetChild(3).position.x);
+    }
+
+    private void FixedUpdate()
+    {
+        float movementSpeed;
+
+        if (Puck.position.y < puckBoundary.Down)
+        {
+            movementSpeed = MaxMovementSpeed * Random.Range(0.1f, 0.3f);
+            targetPosition = new Vector2(Mathf.Clamp(Puck.position.x, playerBoundary.Left, playerBoundary.Right),
+                startingPosition.y);
+        }
+        else
+        {
+            movementSpeed = Random.Range(MaxMovementSpeed * 0.4f, MaxMovementSpeed);
+            targetPosition = new Vector2(Mathf.Clamp(Puck.position.x, playerBoundary.Left, playerBoundary.Right),
+                Mathf.Clamp(Puck.position.y, playerBoundary.Down, playerBoundary.Up));
+            
+           
+        } 
+        
+        rb.MovePosition(Vector2.MoveTowards(rb.position, targetPosition, movementSpeed * Time.fixedDeltaTime));
+    }
+}
--- Space Hockey/Assets/Scripts/PlayerMovement.cs
+++ Space Hockey/Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform BoundaryHolder;
     private Boundary playerBoundary;
-    struct Boundary
-    {
-        public float Up, Down, Left, Right;
-
-        public Boundary(float up, float down, float left, float right)
-        {
-            Up = up;
-            Down = down;
-            Left = left;
-            Right = right;
-        }
-    }
+    
 
     void Start()
     {
